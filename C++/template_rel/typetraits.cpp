ea8c8cc948524837c451eebe8425b34b67989e4b
Lee <lijingqi93@foxmail.com>
2021-09-14 20:12:55 +0800

    enum typetraits

---
+++ C++/template_rel/typetraits.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+enum TESTTYPE {
+    A1,A2,A3,A4
+};
+
+template <TESTTYPE T>
+struct TYPETRAITS{};
+
+template<>
+struct TYPETRAITS<A1>{
+    using MYTYPE = int;
+};
+
+template<>
+struct TYPETRAITS<A2>{
+    using MYTYPE = double;
+};
+
+template<>
+struct TYPETRAITS<A3>{
+    using MYTYPE = long;
+};
+
+
+int main()
+{
+    TYPETRAITS<A1>::MYTYPE a;
+    TYPETRAITS<A2>::MYTYPE b;
+    TYPETRAITS<A3>::MYTYPE c;
+    cout << typeid(a).name() << endl;
+    cout << typeid(b).name() << endl;
+    cout << typeid(c).name() << endl;
+    
+}

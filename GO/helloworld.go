99514c2402c938d5abf006e57f4e918eb1bb6158
MedivhGO <lijingqi93@gmail.com>
2022-03-12 23:26:52 +0800

    add some example code

--- GO/helloworld.go
+++ GO/helloworld.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World")
+
+	for i, r := range "Hello, 世界" {
+		fmt.Printf("%d\t%q\t%d\n", i, r, r)
+	}
+
+	fmt.Println(string(65))
+	fmt.Println(string(123456)) // error unicode code point
+
+	s := "你好"
+	fmt.Printf("% x\n", s)
+	r := []rune(s)
+	fmt.Printf("%x\n", r)
 }

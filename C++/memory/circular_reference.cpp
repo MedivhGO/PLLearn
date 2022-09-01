6bf990f78e17466020b64f787f6c028caaead1c9
MedivhGO <lijingqi93@gmail.com>
2022-09-01 17:08:20 +0800

    add some comment

--- C++/memory/circular_reference.cpp
+++ C++/memory/circular_reference.cpp
@@ -2,28 +2,31 @@
 #include <memory>
 
 using namespace std;
+
 class B;
-class A {
- public:
-  A() : m_sptrB(nullptr){};
-  ~A() { cout << " A is destroyed" << endl; }
-  shared_ptr<B> m_sptrB;
+class A { // A 拥有一个B的指针
+public:
+	A() : m_sptrB(nullptr) {};
+	~A() { cout << " A is destroyed" << endl; }
+	shared_ptr<B> m_sptrB;
 };
-class B {
- public:
-  B() : m_sptrA(nullptr){};
-  ~B() { cout << " B is destroyed" << endl; }
-  shared_ptr<A> m_sptrA;
+
+class B { // B 拥有一个A的指针
+public:
+	B() : m_sptrA(nullptr) {};
+	~B() { cout << " B is destroyed" << endl; }
+	shared_ptr<A> m_sptrA;
 };
 
 int main() {
-  shared_ptr<B> sptrB(new B);         // sptB count 1 有一个B对象，持有A对象的指针。
-  shared_ptr<A> sptrA(new A);         // sptB count 1
-  sptrB->m_sptrA = sptrA;             // sptA count 2
-  cout << sptrA.use_count() << endl;  // 2 sptrA和sptrB 
-  cout << sptrB.use_count() << endl;  // 1 sptrB
-  sptrA->m_sptrB = sptrB;             // sptB count 2 sptrA和sptrB
-  cout << sptrA.use_count() << endl;  // 2
-  cout << sptrB.use_count() << endl;  // 2
-  return 0;
+	shared_ptr<B> sptrB(new B);         // 定义一个sptrB变量
+	shared_ptr<A> sptrA(new A);         // 定义一个sptrA变量
+	cout << sptrA.use_count() << endl;  // 1 sptrA
+	cout << sptrB.use_count() << endl;  // 1 sptrB
+	sptrB->m_sptrA = sptrA;             // sptA count 2, sptrB拥有B, 申请A.
+	sptrA->m_sptrB = sptrB;             // sptB count 2, sptrA拥有A, 申请B.
+	cout << sptrA.use_count() << endl;  // 2
+	cout << sptrB.use_count() << endl;  // 2
+	// 所以直到最后, 资源都没有被释放, 因为这两个shared_ptr的引用计数都不是0
+	return 0;
 }
